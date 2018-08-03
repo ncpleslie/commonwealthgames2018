095ec5469d8c93cb9d1c081422ce15f40dee48f3
nleslie90 <nil0310@arastudent.ac.nz>
2018-08-03 19:49:21 +1200

    Implement webscrape and formatted to json

--- src/Controller.js
+++ src/Controller.js
@@ -94,39 +94,3 @@ class Controller {
     return the2018Games
   }
 }
-
-
-//Turn Webpage into Object
-var wholeTable = $$('tr', $0)
-const APRIL = 3
-var tempObj = []
-
-for (i=1; i<wholeTable.length; i++){
-
-  //Take the names of Pool, TeamA, TeamB. Put into array
-  var newArr = []
-  var eventStr = wholeTable[i].cells[4].innerText
-  newArr = eventStr.split("\n")
-
-  //Take date information. Put into array
-  var dateArr = []
-  var dateStr = wholeTable[i].cells[0].innerText
-  dateArr = dateStr.split(/\W+/gm)
-
-  //Define the object parameters
-  tempObj[i]={Year: 2018, Month: APRIL, Day:"", Hour:"", Minute:"", TeamA:"", TeamB:"", Pool:""}
-
-  //Add team A/B to object
-  tempObj[i].TeamA=newArr[2]
-  tempObj[i].TeamB=newArr[5]
-
-  //Pool comes out as a string. Example "Women Pool A". This takes the last character (The Pool Name) and puts into object
-  var poolStr = newArr[0]
-  var poolStrLast = poolStr.substr(poolStr.length - 1)
-  tempObj[i].Pool=poolStrLast
-
-  //Add date information to the object
-  tempObj[i].Day=parseInt(dateArr[1])
-  tempObj[i].Hour=parseInt(dateArr[3])
-  tempObj[i].Minute=parseInt(dateArr[4])
-}
