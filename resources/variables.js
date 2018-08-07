44c797039a53ab7d60abc81ff2ca81180e416bae
ncpleslie <33884283+ncpleslie@users.noreply.github.com>
2018-08-08 08:11:07 +1200

    Update variables.js

--- resources/variables.js
+++ resources/variables.js
@@ -1,11 +1,11 @@
 var urlObj = [
     'Netball',
     'Gold Coast Convention and Exhibition Centre',
-    "https://cors-anywhere.herokuapp.com/results.gc2018.com/en/netball/event-schedule-women.htm",
+    "https://cryptic-headland-94862.herokuapp.com/results.gc2018.com/en/netball/event-schedule-women.htm",
     'Men\'s Rugby Sevens',
     'Robina Stadium',
-    "https://cors-anywhere.herokuapp.com/results.gc2018.com/en/rugby-sevens/event-schedule-men.htm",
+    "https://cryptic-headland-94862.herokuapp.com/results.gc2018.com/en/rugby-sevens/event-schedule-men.htm",
     'Women\'s Rugby Sevens',
     'Robina Stadium',
-    "https://cors-anywhere.herokuapp.com/results.gc2018.com/en/rugby-sevens/event-schedule-women.htm"
+    "https://cryptic-headland-94862.herokuapp.com/results.gc2018.com/en/rugby-sevens/event-schedule-women.htm"
 ]
