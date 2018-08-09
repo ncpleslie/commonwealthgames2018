fdeba715fc53d3409c260dccb1447605dfdd861b
ncpleslie <33884283+ncpleslie@users.noreply.github.com>
2018-08-09 21:01:18 +1200

    Update variables.js

--- resources/variables.js
+++ resources/variables.js
@@ -1,15 +1,17 @@
 var urlObj = [
     'Netball',
     'Gold Coast Convention and Exhibition Centre',
-    "https://cryptic-headland-94862.herokuapp.com/results.gc2018.com/en/netball/event-schedule-women.htm",
+    "results.gc2018.com/en/netball/event-schedule-women.htm",
     'Men\'s Rugby Sevens',
     'Robina Stadium',
-    "https://cryptic-headland-94862.herokuapp.com/results.gc2018.com/en/rugby-sevens/event-schedule-men.htm",
+    "results.gc2018.com/en/rugby-sevens/event-schedule-men.htm",
     'Women\'s Rugby Sevens',
     'Robina Stadium',
-    "https://cryptic-headland-94862.herokuapp.com/results.gc2018.com/en/rugby-sevens/event-schedule-women.htm"
+    "results.gc2018.com/en/rugby-sevens/event-schedule-women.htm",
+    "https://immense-citadel-58241.herokuapp.com/"
 ]
 // Alternative cos-proxys
+//https://immense-citadel-58241.herokuapp.com/  --This is my own
 //https://cors-anywhere.herokuapp.com/
 //https://cryptic-headland-94862.herokuapp.com/
 //https://stackoverflow.com/questions/47076743/cors-anywhere-herokuapp-com-not-working
